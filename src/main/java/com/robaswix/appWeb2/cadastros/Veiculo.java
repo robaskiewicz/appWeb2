13693050a608cdb0153f9d625970fa75b654610a
Lucas <lukasmarmo@gmail.com>
2021-04-17 15:58:41 -0300

    Marmo - Efetuada visualizacao de pessoas com veiculos no form veiculos

--- src/main/java/com/robaswix/appWeb2/cadastros/Veiculo.java
+++ src/main/java/com/robaswix/appWeb2/cadastros/Veiculo.java
@@ -1,12 +1,16 @@
 package com.robaswix.appWeb2.cadastros;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -36,7 +40,18 @@ public class Veiculo {
 	private Locacao locacao;
 	
 	
+	@OneToMany(
+			cascade= CascadeType.ALL,
+			orphanRemoval = true,
+			mappedBy = "veiculo"
+			)
 	
+	private List<Locacao> locacoes = new ArrayList<>();
+	
+
+	public List<Locacao> getLocacoes() {
+		return locacoes;
+	}
 	
 	
 	@Deprecated
